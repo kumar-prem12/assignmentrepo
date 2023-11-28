5b5617061c1bc1a21692721eba864bef3fa076d2
prem chand <prem72965@gmail.com>
2023-11-28 11:11:59 +0530

    change key

--- secrets.php
+++ secrets.php
@@ -3,4 +3,4 @@
 // or in a private script that does not publicly expose the source code.
 
 // This is your test secret API key.
-$stripeSecretKey = 'sk_test_51O8F0kSIrUytVpfRANiKrC3GXVuQc3SpOgsiKZfVlviBt3L2nq6qAKDqWtbMQzr8rZhZL6k2hbBxKE2iTBkxHeKk00uvJ8bcDW';
+$stripeSecretKey = 'STRIPE-SECRET-API-KEY';
